c049dcf354c236c9aab348206cf2994291e367c1
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-11 04:23:56 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,9 +18,12 @@ def third_challenge
         status: "alive"
         },
       :hero_friends => [
-        name:"Benvolio",
+        {name:"Benvolio",
         age:"17",
-        attitude:"worried"
+        attitude:"worried"},
+        {name:"Mercutio",
+        age:"18",
+        attitude:"hot-headed"}
         ]
    }, 
    :capulet => {
@@ -40,7 +43,14 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {name:"Steven",
+        age:"30",
+        attitude:"confused"},
+        {name:"Nurse",
+        age:"44",
+        attitude:"worried"}
+        ]
    }
   }
 
